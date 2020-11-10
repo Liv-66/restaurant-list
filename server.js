3df765ef8d6e5676c3fc6ccc9462f6ca647e6d54
Liv Huang <livhuang95@gmail.com>
2020-11-10 20:34:16 +0800

    feat: project init

---
+++ server.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
