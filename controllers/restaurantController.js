dc3b79516226799c7101f1c5d8b2a5233bfce7df
Liv Huang <livhuang95@gmail.com>
2020-11-11 00:21:23 +0800

    modify get all restaurants

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,11 +1,15 @@
 const Restaurant = require('../models/restaurantModel');
 
-exports.getAll = (req, res) => {
-  res.end('get all restaurants');
+exports.getAll = async (req, res) => {
+  const restaurants = await Restaurant.find();
+  res.status(200).json({
+    status: 'success',
+    results: restaurants.length,
+    data: restaurants,
+  });
 };
 
 exports.createRestaurant = async (req, res, next) => {
-  console.log(req.body);
   const restaurant = await Restaurant.create(req.body);
   res.status(201).json({
     status: 'success',
