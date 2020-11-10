1ba593da12ea1b9ca747fa48e226255cba3fac3d
Liv Huang <livhuang95@gmail.com>
2020-11-10 20:59:09 +0800

    feat: add user router

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const userRouter = require('./routes/userRouter');
+
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+app.use('/users', userRouter);
 
 module.exports = app;
