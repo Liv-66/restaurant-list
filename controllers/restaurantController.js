37fbce284357a819c24d25ab52a53841403c3b53
Liv Huang <livhuang95@gmail.com>
2020-11-11 00:29:52 +0800

    feat: add get one restaurant

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,12 +1,22 @@
 const Restaurant = require('../models/restaurantModel');
 
-exports.getAll = async (req, res) => {
+exports.getAll = async (req, res, next) => {
   const restaurants = await Restaurant.find();
   res.status(200).json({
     status: 'success',
     results: restaurants.length,
     data: restaurants,
   });
+  next();
+};
+
+exports.getOne = async (req, res, next) => {
+  const restaurant = await Restaurant.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: restaurant,
+  });
+  next();
 };
 
 exports.createRestaurant = async (req, res, next) => {
--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -4,6 +4,7 @@ const restaurantController = require('../controllers/restaurantController');
 const router = express.Router();
 
 router.get('/', restaurantController.getAll);
+router.get('/:id', restaurantController.getOne);
 router.post('/create', restaurantController.createRestaurant);
 
 module.exports = router;
